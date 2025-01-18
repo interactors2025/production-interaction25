3543b4b5340fd2437b6eb4632f1737fbb59b0fee
gurudesh@0801 <gurudesh2204@gmail.com>
2025-01-18 11:14:29 +0530

    changes made by guru

--- client/Frontend-main/src/Components/Sponsers/Sponsors.jsx
+++ client/Frontend-main/src/Components/Sponsers/Sponsors.jsx
@@ -3,35 +3,35 @@ import "./Sponsors.css";
 const Sponsors = () => {
   const sponsors = [
     {
-      name: "Company B",
+      name: "Company A",
       logo: "https://res.cloudinary.com/delnxjp38/image/upload/v1735399926/Bank_of_maha_hpwkux.png",
     },
     {
-      name: "Company C",
+      name: "Company B",
       logo: "https://res.cloudinary.com/delnxjp38/image/upload/v1735399903/Hdfc_bank_xo8syk.jpg",
     },
     {
-      name: "Company E",
+      name: "Company C",
       logo: "https://res.cloudinary.com/delnxjp38/image/upload/v1735363937/coditude_krffx7.jpg",
     },
     {
-      name: "Company F",
+      name: "Company D",
       logo: "https://res.cloudinary.com/delnxjp38/image/upload/v1735363952/Rian_a83vwc.webp",
     },
     {
-      name: "Company G",
+      name: "Company E",
       logo: "https://res.cloudinary.com/delnxjp38/image/upload/v1735364254/jetking_q5cmai.png",
     },
     {
-      name: "Company I",
+      name: "Company F",
       logo: "https://res.cloudinary.com/delnxjp38/image/upload/v1735399977/Edwise_od3s42.avif",
     },
     {
-      name: "Company J",
+      name: "Company I",
       logo: "https://res.cloudinary.com/delnxjp38/image/upload/v1735494116/powerhouse_wjilrx.png",
     },
     {
-      name: "Company K",
+      name: "Company J",
       logo: "https://res.cloudinary.com/delnxjp38/image/upload/v1736397179/Logos/pwur1ovqqdzazslelogw.png",
     },
   ];
